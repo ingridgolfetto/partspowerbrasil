5967cf7dbf6f49f593ca09e8d0ef9edeb74694c2
ingrid <ingridgolfetto@hotmail.com>
2025-04-01 15:02:41 -0300

    fix: ajuste de path imagens

--- src/app/enums/produtos.ts
+++ src/app/enums/produtos.ts
@@ -7,62 +7,62 @@ export const PRODUTOS: Produtos[] = [
       name: 'Produto 1',
       category: Categorias.johnDeerer.name,
       subcategories: ['Sub1'],
-      imageUrl: '/produtos/1.png'
+      imageUrl: './../../../assets/imgs/produtos/1.png'
     },
     {
       id: 2,
       name: 'Produto 2',
       category: Categorias.masseyFerguson.name,
       subcategories: ['Sub1'],
-      imageUrl: '/produtos/2.png'
+      imageUrl: './../../../assets/imgs/produtos/2.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
       subcategories: ['Sub5'],
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     },
     {
       id: 3,
       name: 'Produto 3',
       category: Categorias.compressores.name,
-      imageUrl: '/produtos/3.png'
+      imageUrl: './../../../assets/imgs/produtos/3.png'
     }
   ];
--- src/app/enums/slides.enum.ts
+++ src/app/enums/slides.enum.ts
@@ -2,37 +2,37 @@ import { ISlide } from '../interface/slide';
 
 export const SLIDES: ISlide[] = [
   {
-    imagePath: '/produtos/1.png',
+    imagePath: './../../../assets/imgs/produtos/1.png',
     id: 1,
     descricao: 'Descrição do Produto 1'
   },
   {
-    imagePath: '/produtos/2.png',
+    imagePath: './../../../assets/imgs/produtos/2.png',
     id: 2,
     descricao: 'Descrição do Produto 2'
   },
   {
-    imagePath: '/produtos/3.png',
+    imagePath: './../../../assets/imgs/produtos/3.png',
     id: 3,
     descricao: 'Descrição do Produto 3'
   },
   {
-    imagePath: '/produtos/4.png',
+    imagePath: './../../../assets/imgs/produtos/4.png',
     id: 4,
     descricao: 'Descrição do Produto 4'
   },
   {
-    imagePath: '/produtos/5.png',
+    imagePath: './../../../assets/imgs/produtos/5.png',
     id: 5,
     descricao: 'Descrição do Produto 5'
   },
   {
-    imagePath: '/assets/imgs/produto6.jpg',
+    imagePath: './../../../assets/imgs/produtos/6.jpg',
     id: 6,
     descricao: 'Descrição do Produto 6'
   },
   {
-    imagePath: '/assets/imgs/produto7.jpg',
+    imagePath: './../../../assets/imgs/produtos/7.jpg',
     id: 7,
     descricao: 'Descrição do Produto 7'
   }
